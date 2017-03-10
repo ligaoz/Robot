b1154f66d73603c476439aa4c541ed17480921c6
Liga Ozolina <liga.ozolina@ucdconnect.ie>
2017-03-10 00:09:33 +0000

    data parsing and create_world functions added

--- robot/main.py
+++ robot/main.py
@@ -3,6 +3,20 @@ Created on 9 Mar 2017
 
 @author: liga
 '''
+import argparse
+from urllib import urlopen
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', help='input help')
+    arguments = parser.parse_args()
+    url = arguments.input
+    html = urlopen(url)
+    data = html.read().decode('utf-8').split("\n")
+    print(data)
+   # size = int(data[0])
+   # print(size)
+   # data.pop(0)
 
 if __name__ == '__main__':
-    pass
+    main()
--- robot/world.py
+++ robot/world.py
@@ -3,6 +3,33 @@ Created on 9 Mar 2017
 
 @author: liga
 '''
+from __builtin__ import file
 
-if __name__ == '__main__':
-    pass
+    
+def create_world(data,size):
+    """Function to create multidimensional array to represent world
+     Function also checks if size input is valid which is size>0"""
+    def parseLine(data):
+        """Function to parse data file"""
+        cleanData = []
+        for line in data:
+            line = line.strip()
+            line = line.replace(",", " ")
+            cleanData += [line.split(), ]
+            print(line)
+        return cleanData
+    
+    def add_walls(x,y,matrix):
+        """function to add the walls in the world"""
+        matrix[x][y] = 1
+        return matrix
+     
+    if size > 0:
+        matrix = [[0 for x in range(size)] for x in range(size)]
+        for line in file:
+            x = line[1]
+            y = line[2]
+            matrix = add_walls(x,y,matrix)
+        return matrix
+    else:
+        return False
